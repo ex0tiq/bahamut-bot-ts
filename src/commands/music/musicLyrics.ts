f65e1ee2b226063c271184fe0f335f3f28c59ee5
Christian Holzwarth <christian@holzwarth.family>
2022-11-11 10:44:36 +0100

    Implement lyrics command

---
+++ src/commands/music/musicLyrics.ts
@@ -0,0 +1,178 @@
+
+import {CommandType} from "wokcommands";
+import BahamutClient from "../../modules/BahamutClient";
+import Discord from "discord.js";
+import {getGuildSettings} from "../../lib/getFunctions";
+import {BahamutCommandPreChecker, PreCheckType} from "../../modules/BahamutCommandPreChecker";
+import {handleErrorResponseToMessage, handleResponseToMessage} from "../../lib/messageHandlers";
+import emoji from "node-emoji";
+// Not ES compatible imports
+const Genius = require('genius-lyrics');
+
+const config = {
+    name: 'lyrics',
+    aliases: ['ap', 'auto'],
+    type: CommandType.LEGACY,
+    testOnly: false,
+    description: 'Get the lyrics to the currently playing song or search for another song',
+    expectedArgs: '[song name]',
+    minArgs: 0,
+    category: 'Music',
+    cooldown: '10s',
+    deferReply: true,
+    guildOnly: true,
+};
+
+export default {
+    ...config,
+    callback: async ({
+                         client,
+                         message,
+                         channel,
+                         args,
+                         member,
+                         interaction
+                     }: { client: BahamutClient, message: Discord.Message, channel: Discord.TextChannel, member: Discord.GuildMember, args: string[], interaction: Discord.CommandInteraction }) => {
+        const settings = await getGuildSettings(client, channel.guild);
+        // Abort if module is disabled
+        if (settings.disabled_categories.includes('music')) return;
+
+        const GeniusClient = new Genius.Client(client.bahamut.config.genius_token);
+
+        if (args.length === 0) {
+            const checks = new BahamutCommandPreChecker(client, {
+                client,
+                message,
+                channel,
+                args,
+                member,
+                interaction
+            }, config, [
+                {type: PreCheckType.USER_IN_VOICE_CHANNEL},
+                {type: PreCheckType.MUSIC_NODES_AVAILABLE}
+            ]);
+            if (await checks.runChecks()) return;
+
+            const player = client.bahamut.musicHandler.manager.create({
+                guild: channel.guild.id,
+                textChannel: channel.id,
+            });
+
+            if (!player.playing) return handleErrorResponseToMessage(client, message || interaction, false, config.deferReply, `${emoji.get('control_knobs')} There is nothing playing at the moment!`);
+            if (player.queue.current!.isStream) return handleErrorResponseToMessage(client, message || interaction, false, config.deferReply, 'Lyrics cannot be searched for webstreams!');
+
+            const search = await searchLyrics(client, GeniusClient, player.queue.current!.title);
+
+            if (!search || search.result === null) {
+                return handleErrorResponseToMessage(client, message || interaction, false, config.deferReply, `Sorry, I couldn't find any lyrics for \`${player.queue.current!.title}\`.`);
+            }
+            else {
+                const templyrics = search.lyrics.replace(/\n/g, '%b');
+                if (templyrics.length >= 2000) {
+                    await handleErrorResponseToMessage(
+                        client,
+                        message || interaction,
+                        false,
+                        config.deferReply,
+                        `**Lyrics for ${search.result.fullTitle}**`
+                    );
+
+                    const temp = templyrics.match(/.{1,1950}(\s|$)/g);
+
+                    for (const t of temp) {
+                        if (message) await message.channel.send(t.trim().replace(/%b/g, '\n'));
+                        else interaction.channel?.send(t.trim().replace(/%b/g, '\n'));
+                    }
+
+                    if (message) await message.channel.send('*Powered by Genius*');
+                    else interaction.channel?.send('*Powered by Genius*');
+                    return;
+                }
+                else {
+                    return handleResponseToMessage(
+                        client,
+                        message || interaction,
+                        false,
+                        config.deferReply,
+                        {
+                            embeds: [
+                                new Discord.EmbedBuilder()
+                                    .setTitle(`Lyrics for ${search.result.fullTitle}`)
+                                    .setDescription(search.lyrics)
+                                    .setFooter({ text: 'Powered by Genius' })
+                            ]
+                        }
+                    )
+                }
+            }
+        }
+        else {
+            const search = await searchLyrics(client, GeniusClient, args.join(' '));
+
+            if (!search || search.result === null) {
+                return handleErrorResponseToMessage(client, message || interaction, false, config.deferReply, `Sorry, I couldn't find any lyrics for \`${args.join(" ")}\`.`);
+            }
+
+            const templyrics = search.lyrics.replace(/\n/g, '%b');
+            if (templyrics.length >= 2000) {
+                await handleErrorResponseToMessage(
+                    client,
+                    message || interaction,
+                    false,
+                    config.deferReply,
+                    `**Lyrics for ${search.result.fullTitle}**`
+                );
+
+                const temp = templyrics.match(/.{1,1950}(\s|$)/g);
+
+                for (const t of temp) {
+                    if (message) await message.channel.send(t.trim().replace(/%b/g, '\n'));
+                    else interaction.channel?.send(t.trim().replace(/%b/g, '\n'));
+                }
+
+                if (message) await message.channel.send('*Powered by Genius*');
+                else interaction.channel?.send('*Powered by Genius*');
+                return;
+            }
+            else {
+                return handleResponseToMessage(
+                    client,
+                    message || interaction,
+                    false,
+                    config.deferReply,
+                    {
+                        embeds: [
+                            new Discord.EmbedBuilder()
+                                .setTitle(`Lyrics for ${search.result.fullTitle}`)
+                                .setDescription(search.lyrics)
+                                .setFooter({ text: 'Powered by Genius' })
+                        ]
+                    }
+                )
+            }
+        }
+    },
+};
+
+const searchLyrics = async (client: BahamutClient, geniusClient: unknown, searchTerm: string) => {
+    try {
+        // @ts-ignore
+        const searches = await geniusClient.songs.search(searchTerm),
+            searchResult = searches[0] || null;
+
+        if (searchResult == null) {
+            return null;
+        }
+        else {
+            return {
+                lyrics: await (searchResult.lyrics()),
+                result: searchResult,
+            };
+        }
+    }
+    catch (err) {
+        console.error("Error while searching for song lyrics:", err);
+        return null;
+    }
+};
+
