58a87d9f3f7bba0fef70730c3671fe56305adeb7
Christian Holzwarth <christian@holzwarth.family>
2022-11-12 01:38:41 +0100

    Restore partial messageCreate event

--- src/events/messageCreate/isHuman/messageCreate.ts
+++ src/events/messageCreate/isHuman/messageCreate.ts
@@ -2,12 +2,12 @@
 //const AutoMod = require('../../../lib/automoderation/autoModeration');
 //const RestrictedChannels = require('../../../lib/automoderation/restrictedChannels');
 import BahamutClient from "../../../modules/BahamutClient";
-
-const { flattenArray } = require('../../../lib/toolFunctions');
+import { flattenArray } from '../../../lib/toolFunctions';
 import Discord from "discord.js";
 import WOK from "wokcommands";
+import {getGuildSettings} from "../../../lib/getFunctions";
+import {handleResponseToMessage} from "../../../lib/messageHandlers";
 
-// eslint-disable-next-line no-unused-vars
 export default async (message: Discord.Message, client: BahamutClient, instance: WOK) => {
     // Implement auto moderation
     //const mod = new AutoMod(client, message.guild, message);
@@ -20,24 +20,27 @@ export default async (message: Discord.Message, client: BahamutClient, instance:
 
     if (message.author.bot) return;
 
-    //const settings = await client.getSettings(message.guild);
+    const settings = await getGuildSettings(client, message.guild!);
 
     // Checks if the bot was mentioned, with no message after it, returns the prefix.
-    //const prefixMention = new RegExp(`^<@!?${client.user.id}>( |)$`);
-    //if (message.content.match(prefixMention)) {
-    //    return handleBotMessage(client, message, 'info', {
-    //        title: 'Prefix',
-    //        desc: `The current bot prefix on this server is \`${settings.prefix}\`.`,
-    //    });
-    //}
+    const prefixMention = new RegExp(`^<@!?${client.user!.id}>( |)$`);
+    if (message.content.match(prefixMention)) {
+        return handleResponseToMessage(client, message, false, true, {
+            embeds: [
+                new Discord.EmbedBuilder()
+                    .setTitle('Prefix')
+                    .setDescription(`The current bot prefix on this server is \`${settings.prefix}\`.`)
+            ]
+        })
+    }
 
     // If the member on a guild is invisible or not cached, fetch them.
-    //if (message.guild && !message.member) {
-    //    await message.guild.fetchMember(message.author);
-    //}
+    if (message.guild && !message.member) {
+        await message.guild.members.fetch(message.author);
+    }
 
-    //const commands = flattenArray(Array.from(client.cmdHandler.commandHandler.commands).map(([key,]) => key));
-    //if (!message.content.startsWith(settings.prefix) && !commands.includes(message.content.split(' ')[0].replace(settings.prefix, ''))) {
-    //    await client.levelSystem.handleNewUserMessage(client, message, message.author);
-    //}
+    const commands = flattenArray([...client.bahamut.cmdHandler.commandHandler.commands].map(([key,]) => key));
+    if (!message.content.startsWith(settings.prefix) && !commands.includes(message.content.split(' ')[0].replace(settings.prefix, ''))) {
+        await client.bahamut.levelSystem.handleNewUserMessage(message, message.member!);
+    }
 };
