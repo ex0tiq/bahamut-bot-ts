db7288cb165586f277d2832c87bd30b0d6fab24b
Christian Holzwarth <christian@holzwarth.family>
2022-11-13 23:54:06 +0100

    Remove unused functions

--- src/lib/parseFunctions.ts
+++ src/lib/parseFunctions.ts
@@ -1,36 +1,16 @@
-
 /**
  * Parse a string to a boolean value
  * @param {string} string
  * @returns {null|boolean}
  */
 const parseBool = (string: string) => {
-    if ((string.toLowerCase() === 'true') || (string.toLowerCase() === '1')) {
+    if ((string.toLowerCase() === "true") || (string.toLowerCase() === "1")) {
         return true;
-    }
-    else if ((string.toLowerCase() === 'false') || (string.toLowerCase() === '0')) {
+    } else if ((string.toLowerCase() === "false") || (string.toLowerCase() === "0")) {
         return false;
-    }
-    else {
+    } else {
         return null;
     }
 };
 
-const hexToRGB = (hex: string, alpha: number) => {
-    const r = parseInt(hex.slice(1, 3), 16),
-        g = parseInt(hex.slice(3, 5), 16),
-        b = parseInt(hex.slice(5, 7), 16);
-
-    if (alpha) {
-        return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + alpha + ')';
-    }
-    else {
-        return 'rgb(' + r + ', ' + g + ', ' + b + ')';
-    }
-};
-
-const hexToDecimal = (hex: string) => {
-    return parseInt(hex, 16);
-}
-
-export { hexToDecimal, hexToRGB, parseBool }
+export { parseBool };
