e07af15500b36522dfe0cf3123dad95bdfdb3ee9
Christian Holzwarth <christian@holzwarth.family>
2022-11-14 20:53:43 +0100

    Import cookieslots command

---
+++ src/commands/fun/cookies/cookieslots.ts
@@ -0,0 +1,79 @@
+import { randomIntBetween } from "../../../lib/toolFunctions";
+import { CommandConfig } from "../../../../typings";
+import { CommandType } from "wokcommands";
+import BahamutClient from "../../../modules/BahamutClient";
+import Discord from "discord.js";
+import { handleErrorResponseToMessage, handleResponseToMessage } from "../../../lib/messageHandlers";
+const { generateSlotsEmbed } = require("../../../lib/slotsFunctions");
+const { isInt } = require("../../../lib/validateFunctions");
+
+const config: CommandConfig = {
+    name: "cookieslots",
+    aliases: ["slots"],
+    type: CommandType.LEGACY,
+    testOnly: false,
+    description: "Play the slot machine with your cookies.",
+    category: "Fun",
+    guildOnly: true,
+    deferReply: true,
+};
+
+export default {
+    ...config,
+    callback: async ({ client, message, channel, interaction, member }: { client: BahamutClient, message: Discord.Message, channel: Discord.TextChannel, interaction: Discord.CommandInteraction, member: Discord.GuildMember }) => {
+        const cookies = await client.bahamut.dbHandler.cookie.getDBUserCookies(channel.guild, member),
+            // eslint-disable-next-line no-useless-escape
+            emojis = ["\:cherries:", "\:watermelon:", "\:tangerine:", "\:strawberry:", "\:grapes:", "\:kiwi:"];
+
+        if (!cookies || (isInt(cookies) && cookies === 0)) return handleErrorResponseToMessage(client, message || interaction, false, config.deferReply, "You don't have any cookies! You need at least **10** cookies to play the slot machine!");
+        if (isInt(cookies) && (cookies) < 10) return handleErrorResponseToMessage(client, message || interaction, false, config.deferReply, "You don't have enough cookies! You need at least **10** cookies to play the slot machine!");
+
+        handleResponseToMessage(client, message || interaction, false, config.deferReply, { embeds: [generateSlotsEmbed(client, emojis, [])] }).then((msg) => {
+            const resultEmojis: string[] = [];
+            for (let i = 0; i < 3; i++) {
+                resultEmojis.push(emojis[randomIntBetween(0, 2)]);
+            }
+
+            setTimeout(async () => {
+                const embed = generateSlotsEmbed(client, emojis, resultEmojis);
+
+                if (resultEmojis[0] === resultEmojis[1] && resultEmojis[0] === resultEmojis[2]) {
+                    if (await client.bahamut.dbHandler.cookie.addDBCookiesToUser(channel.guild, member, 100)) {
+                        await handleResponseToMessage(client, msg, true, config.deferReply, {
+                            // eslint-disable-next-line no-useless-escape
+                            content: `${message.author} spent **10** \:cookie: to play the slots... and won, big time! \:smiley:\n**100** \:cookie: cookies have been added to your account!`,
+                            embeds: [embed],
+                        });
+                        return;
+                    } else {
+                        return handleErrorResponseToMessage(client, message || interaction, false, config.deferReply, "An error occurred while doing that. Please try again later.");
+                    }
+                } else if (resultEmojis[0] === resultEmojis[1] || resultEmojis[0] === resultEmojis[2] || resultEmojis[1] === resultEmojis[2]) {
+                    if (await client.bahamut.dbHandler.cookie.setDBUserCookieData(channel.guild, member, 10)) {
+                        await handleResponseToMessage(client, msg, true, config.deferReply, {
+                            // eslint-disable-next-line no-useless-escape
+                            content: `${message.author} spent **10** \:cookie: to play the slots... and almost won! \:neutral_face:`,
+                            embeds: [embed],
+                        });
+                        return;
+                    } else {
+                        return handleErrorResponseToMessage(client, message || interaction, false, config.deferReply, "An error occurred while doing that. Please try again later.");
+                    }
+                } else if (await client.bahamut.dbHandler.cookie.subDBCookiesFromUser(channel.guild, member, 10)) {
+                    await handleResponseToMessage(client, msg, true, config.deferReply, {
+                        // eslint-disable-next-line no-useless-escape
+                        content: `${message.author} spent **10** \:cookie: to play the slots... and lost! \:slight_frown:`,
+                        embeds: [embed],
+                    });
+                    return;
+                } else {
+                    return handleErrorResponseToMessage(client, message || interaction, false, config.deferReply, "An error occurred while doing that. Please try again later.");
+                }
+            }, 3000);
+        }).catch((err) => {
+            console.error("Error while playing cookie slots:", err);
+            return handleErrorResponseToMessage(client, message || interaction, false, config.deferReply, "An error occurred while doing that. Please try again later.");
+        });
+        return;
+    },
+};
---
+++ src/lib/slotsFunctions.ts
@@ -0,0 +1,67 @@
+import { randomIntBetween } from "./toolFunctions";
+import BahamutClient from "../modules/BahamutClient";
+import Discord from "discord.js";
+
+/**
+ * Generate the embed message for the slot machine
+ * @param client
+ * @param message
+ * @param emojis
+ * @param resultEmojis
+ */
+const generateSlotsEmbed = (client: BahamutClient, emojis = [], resultEmojis = []) => {
+    const objEmojiAnim = [
+        { "slots4_anim": "809832919538335854" },
+        { "slots3_anim": "809832919471357982" },
+        { "slots2_anim": "809832919509630976" },
+        { "slots1_anim": "809832919542661120" },
+    ], emojiAnim = objEmojiAnim.map((e) => {
+        return `<a:${Object.keys(e)[0]}:${Object.values(e)[0]}>`;
+    });
+    if (resultEmojis.length < 3) {
+        return new Discord.EmbedBuilder()
+            .setAuthor({ name: "Slot Machine", iconURL: client.bahamut.config.cookie_images.slots_icon })
+            .setDescription(`${emojiAnim[0]}\t\t | ${emojiAnim[2]} | ${emojiAnim[3]}
+			${emojiAnim[1]} | ${emojiAnim[3]} | ${emojiAnim[2]}
+			${emojiAnim[2]} | ${emojiAnim[1]} | ${emojiAnim[0]}
+			---------------
+			${emojiAnim[3]} | ${emojiAnim[0]} | ${emojiAnim[1]}
+			---------------
+		`);
+    } else if (resultEmojis.length === 3) {
+        const firstRow: string[] = [], secondRow: string[] = [], thirdRow: string[] = [];
+
+        while (firstRow.length <= 3) {
+            const rand = emojis[randomIntBetween(0, 5)];
+            if ((rand !== resultEmojis[0]) && !firstRow.includes(rand)) {
+                firstRow.push(rand);
+            }
+        }
+        while (secondRow.length <= 3) {
+            const rand = emojis[randomIntBetween(0, 5)];
+            if ((rand !== resultEmojis[1]) && !secondRow.includes(rand)) {
+                secondRow.push(rand);
+            }
+        }
+        while (thirdRow.length <= 3) {
+            const rand = emojis[randomIntBetween(0, 5)];
+            if ((rand !== resultEmojis[2]) && !thirdRow.includes(rand)) {
+                thirdRow.push(rand);
+            }
+        }
+
+        return new Discord.EmbedBuilder()
+            .setAuthor({ name: "Slot Machine", iconURL: client.bahamut.config.cookie_images.slots_icon })
+            .setDescription(`${firstRow[0]} | ${secondRow[0]} | ${thirdRow[0]}
+			${firstRow[1]} | ${secondRow[1]} | ${thirdRow[1]}
+			${firstRow[2]} | ${secondRow[2]} | ${thirdRow[2]}
+			---------------
+			${resultEmojis[0]} | ${resultEmojis[1]} | ${resultEmojis[2]}
+			---------------
+		`);
+    } else {
+        return null;
+    }
+};
+
+export { generateSlotsEmbed };
