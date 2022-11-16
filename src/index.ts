c402a8b4f3532c6425748d5ecda5806765fb196a
Christian Holzwarth <christian@holzwarth.family>
2022-11-16 18:07:41 +0100

    Import BotAPIHandler

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,24 @@
-import {BahamutShardingBootManager, BootConfig, BootManager, BotAPIConfig, BotStaticConfig} from "../typings.js";
+import { BahamutShardingBootManager, BootConfig, BootManager, BotAPIConfig, BotStaticConfig } from "../typings.js";
 
-process.env.TZ = 'UTC';
+process.env.TZ = "UTC";
 
 // Include discord.js ShardingManger
 import Discord from "discord.js";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
-//const ShardingManager = require('./modules/ShardingManager');
-const config = require('../config/config.json');
-const apiConfig = require('../config/api_config.json');
+// const ShardingManager = require('./modules/ShardingManager');
+const config = require("../config/config.json");
+const apiConfig = require("../config/api_config.json");
 import logger from "./modules/Logger.js";
 
-//const BotApiHandler = require('./modules/BotAPIHandler');
+// const BotApiHandler = require('./modules/BotAPIHandler');
 import UplinkAPIHandler from "./modules/UplinkAPIHandler.js";
-//const FFXIVSchedulers = require('./modules/ShardManFFXIVSchedulers');
-//const DB = require('./modules/ShardManDatabase');
-//const { isInt } = require('./lib/validateFunctions');
+// const FFXIVSchedulers = require('./modules/ShardManFFXIVSchedulers');
+// const DB = require('./modules/ShardManDatabase');
+// const { isInt } = require('./lib/validateFunctions');
 import { isInt } from "./lib/validateFunctions.js";
 import BahamutShardingManager from "./modules/BahamutShardingManager.js";
+import BotAPIHandler from "./modules/BotAPIHandler";
 
 // Use bluebird as global promise library
 // import * as Promise from "bluebird";
@@ -39,25 +40,25 @@ async function startup() {
 
     // Start UplinkAPIHeartbeat
     const uplinkApiHandler = new UplinkAPIHandler(bootManager, communication_token);
-    logger.log('SM', 'Requesting boot configuration from C&C server...');
+    logger.log("SM", "Requesting boot configuration from C&C server...");
     // Init uplink api heartbeat and fetch boot conf
     try {
         bootConf = <BootConfig> await uplinkApiHandler.initHeartbeat(false, true);
     } catch {
-        logger.error('SM', 'Failed to fetch boot configuration, shutting down...');
+        logger.error("SM", "Failed to fetch boot configuration, shutting down...");
         process.exit(0);
     }
 
     if (!bootConf) {
-        logger.error('SM', 'Failed to fetch boot configuration, shutting down...');
+        logger.error("SM", "Failed to fetch boot configuration, shutting down...");
         process.exit(0);
     }
     if (!bootConf.token) {
-        logger.error('SM', 'Incomplete boot configuration received, shutting down...');
+        logger.error("SM", "Incomplete boot configuration received, shutting down...");
         process.exit(0);
     }
 
-    logger.log('SM', 'Boot configuration received, continuing startup...');
+    logger.log("SM", "Boot configuration received, continuing startup...");
 
     bootManager.config = {
         ...bootManager.config,
@@ -67,7 +68,7 @@ async function startup() {
     logger.log("SM", "Booting with token: " + bootConf.token);
 
     // Create ShardingManger instance
-    let manager = new BahamutShardingManager('./dist/bahamut.js', {
+    const manager = new BahamutShardingManager("./dist/bahamut.js", {
         // 'auto' handles shard count automatically
         totalShards: ((bootConf.total_shards !== "auto") ? parseInt(bootConf.total_shards) : "auto"),
 
@@ -75,42 +76,42 @@ async function startup() {
         token: bootConf.token,
 
         // Stringify bootconf
-        shardArgs: [JSON.stringify(bootConf)]
+        shardArgs: [JSON.stringify(bootConf)],
     }, uplinkApiHandler);
 
     // Combine ShardingManager with bootManager
-    let botManager: BahamutShardingBootManager = Object.assign(manager, bootManager);
+    const botManager: BahamutShardingBootManager = Object.assign(manager, bootManager);
     // Start db connection
-    //manager.dbHandler = new DB(manager),
+    // manager.dbHandler = new DB(manager),
     // Set new api manager
-    //manager.uplinkApiHandler = uplinkApiHandler;
+    // manager.uplinkApiHandler = uplinkApiHandler;
     botManager.uplinkApiHandler.setManager(botManager);
 
     if (!(await botManager.uplinkApiHandler.isApiReachable())) {
-        logger.error('SM', 'Uplink API not reachable, shutting down');
+        logger.error("SM", "Uplink API not reachable, shutting down");
         process.exit(0);
         return;
     }
 
     // Start ApiHandler server
-    //botManager.apiHandler = new BotApiHandler(manager, communication_token);
+    botManager.apiHandler = new BotAPIHandler(botManager, communication_token);
 
     // The shardCreate event is emitted when a shard is created.
     // You can use it for something like logging shard launches.
-    botManager.on('shardCreate', async (shard) => {
-        shard.on('spawn', async () => {
+    botManager.on("shardCreate", async (shard) => {
+        shard.on("spawn", async () => {
             // Sending the data to the shard. Required for successful shard spawn
-            await shard.send({ type: 'startupData', data: { shardId: shard.id } });
+            await shard.send({ type: "startupData", data: { shardId: shard.id } });
         });
-        shard.on('ready', () => {
-            if (!botManager.shardReady) manager.emit('shardReady');
+        shard.on("ready", () => {
+            if (!botManager.shardReady) manager.emit("shardReady");
         });
 
-        logger.log('SM', `Shard ${shard.id} launched`);
+        logger.log("SM", `Shard ${shard.id} launched`);
     });
 
     // @ts-ignore
-    botManager.on('shardReady', async () => {
+    botManager.on("shardReady", async () => {
         botManager.shardReady = true;
         // Start heartbeat
         await botManager.uplinkApiHandler.initHeartbeat(false, false);
@@ -120,6 +121,6 @@ async function startup() {
     await botManager.spawn();
 
     // Register schedulers
-    //new FFXIVSchedulers(botManager);
+    // new FFXIVSchedulers(botManager);
 }
 
---
+++ src/modules/BotAPIHandler.ts
@@ -0,0 +1,165 @@
+import express from "express";
+import logger from "./Logger";
+import BahamutClient from "./BahamutClient";
+import { BahamutShardingBootManager } from "../../typings";
+
+export default class BotAPIHandler {
+    private shardManager: BahamutShardingBootManager;
+    private srv;
+    private listener;
+    private readonly communication_token: string;
+    private readonly apiPort: number;
+
+    constructor(shardManager: BahamutShardingBootManager, communication_token: string) {
+        this.srv = express();
+        this.shardManager = shardManager;
+        this.communication_token = communication_token;
+        this.apiPort = this.shardManager.apiConfig.api_port;
+
+        // Support for json bodies (post)
+        this.srv.use(express.json());
+        // Support static files
+        this.srv.use(express.static("assets"));
+        // Require valid api key for every request
+        this.srv.use((req, res, next) => {
+            res.header("Content-Type", "application/json");
+
+            let ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+            if (ip?.includes(":")) {
+                const t = Array.isArray(ip) ? ip[0].split(":") : ip.split(":");
+                ip = t.slice(t.length - 2);
+                ip = ip.length > 1 ? ip[1] : ip[0];
+            }
+
+            // Abort if no shard available for requests
+            if (!this.shardManager.shardReady) {
+                res.status(503);
+                res.end(JSON.stringify({
+                    status: "error",
+                    message: "No server available for request handling.",
+                    result: null,
+                }));
+                return;
+            }
+
+            if (!this.shardManager.config.uplink_api_allowed_ips.includes((Array.isArray(ip) ? ip.join("") : ip) || "") || !req.query || !req.query.communication_token || this.communication_token !== req.query.communication_token) {
+                logger.error("SM", `API request from IP ${ip} denied!`);
+                res.status(403);
+                res.end(JSON.stringify({
+                    status: "error",
+                    message: "Access denied",
+                    result: null,
+                }));
+                return;
+            } else {
+                next();
+            }
+        });
+        // Abort if invalid json found
+        // eslint-disable-next-line no-unused-vars
+        this.srv.use((err: any, req: any, res: { status: (arg0: number) => void; end: (arg0: string) => void; }, next: any) => {
+            console.log(err);
+            // 'SyntaxError: Unexpected token n in JSON at position 0'
+            res.status(400);
+            res.end(JSON.stringify({
+                status: "error",
+                message: "Bad request",
+                result: null,
+            }));
+            return;
+        });
+
+        this.registerHandlers();
+
+        this.listener = this.srv.listen(this.apiPort, () => {
+            // @ts-ignore
+            logger.ready("SM", `API is listening on port: ${this.listener.address()!.port}`);
+        });
+    }
+
+    registerHandlers() {
+        this.srv.get("/", (req, res) => {
+            res.end(JSON.stringify({
+                status: "success",
+                message: "",
+                result: `Bahamut v${process.env.npm_package_version} ready!`,
+            }));
+            return;
+        });
+
+        this.srv.post("/broadcast", async (req, res) => {
+            if (!req.body.code) {
+                res.status(400);
+                res.end(JSON.stringify({
+                    status: "error",
+                    message: "Bad request",
+                    result: null,
+                }));
+                return;
+            }
+
+            const result = await this.shardManager.broadcastEval((_client: BahamutClient, obj) => {
+                if (obj.shard && _client.shardId !== obj.shard) return null;
+                if (!obj.guild || (obj.guild && _client.guilds.cache.has(obj.guild))) {
+                    const code = `
+					const c = ${obj.code}; 
+					c();`;
+
+                    // DANGEROUS!!
+                    return eval(`${code}`);
+                } else {
+                    return null;
+                }
+            }, { shard: req.body.shard, context: { shard: req.body.shard, guild: req.body.guild, code: req.body.code, ...req.body.additionalContext } });
+
+            const r = {
+                status: "success",
+                message: "",
+                result: result,
+            };
+            if (req.query.includeTimeInformation) {
+                r.result = {
+                    timeInfo: {
+                        localTime: Date.now(),
+                        startupTime: this.shardManager.startTime,
+                    },
+                    origResult: result,
+                };
+            }
+
+            res.end(JSON.stringify(r));
+        });
+
+        this.srv.post("/shardmanEval", async (req, res) => {
+            if (!req.body.code) {
+                res.status(400);
+                res.end(JSON.stringify({
+                    status: "error",
+                    message: "Bad request",
+                    result: null,
+                }));
+                return;
+            }
+
+            let result = eval(`(${req.body.code})`);
+            result = (req.body.code.startsWith("async") ? await result() : result());
+
+            res.end(JSON.stringify({
+                status: "success",
+                message: "",
+                result: result,
+            }));
+            return;
+        });
+
+        this.srv.get("*", (req, res) => {
+            res.status(404);
+            res.end(JSON.stringify({
+                status: "error",
+                message: "Resource not found",
+                result: null,
+            }));
+            return;
+        });
+    }
+}
